d1ea6ec4422112eefc95a246d63f4597f80c7794
Nathan Malmberg <nathan.malmberg@okbu.edu>
2020-06-04 15:09:18 -0500

    Refactor to an object-oriented interface.
    The interface has been rewritten to create the interface from five
    objects: a spectrometer frame, a file frame, a plotting frame,
    a window to create new CSV files and a master window to hold the
    three frames in a notebook. The interface looks the same, but should
    be easier to keep track of the different variables and functions
    required for the program to function. The variables for wavelength,
    duration and frequency were also changed to reflect their numeric
    nature, and the file opening/creating functions were also changed
    to only modify the filename if the file is actually created (as
    opposed to a cancellation of the file creation process), or if the
    preexisting file passes the checks for the program.

--- kineticsgui.py
+++ kineticsgui.py
@@ -21,194 +21,214 @@ from csv import Sniffer, DictReader, DictWriter
 # from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 # from matplotlib.figure import Figure
 
-root = Tk()
-root.title("Absorbance Kinetics")
+class SpecTab():
+    def __init__(self, parent):
+        self.parent = parent
+        self.frame = ttk.Frame(self.parent.notebook)
+        self.parent.notebook.add(self.frame, text="Spectrometer")
+        ttk.Label(self.frame, text="Spectrometer").grid(row=0,column=0)
+        self.speccombo = ttk.Combobox(self.frame)
+        self.speccombo['values'] = ('/dev/usb.serial', '/dev/ttyS0')
+        self.speccombo.bind('<<ComboboxSelected>>', self.specselect)
+        self.speccombo.grid(row=0,column=1)
+        self.wavelength = IntVar()
+        ttk.Label(self.frame, text="Wavelength (nm)").grid(row=1,column=0)
+        self.waveentry = ttk.Entry(self.frame, textvariable=self.wavelength)
+        self.waveentry.grid(row=1,column=1)
+        self.frequency = DoubleVar()
+        ttk.Label(self.frame, text="Frequency (s)").grid(row=2,column=0)
+        self.freqentry = ttk.Entry(self.frame, textvariable=self.frequency)
+        self.freqentry.grid(row=2,column=1)
+        self.duration = DoubleVar()
+        ttk.Label(self.frame, text="Duration (s)").grid(row=3, column=0)
+        self.durentry = ttk.Entry(self.frame, textvariable=self.duration)
+        self.durentry.grid(row=3,column=1)
+        self.blankbutton = ttk.Button(self.frame, text="Blank",
+                                      command=self.blank)
+        self.blankbutton.grid(row=4,column=0)
+        self.runbutton = ttk.Button(self.frame, text="Run",
+                                    command=self.collect)
+        self.runbutton.grid(row=4,column=1)
+        self.runprogress = DoubleVar()
+        self.progress = ttk.Progressbar(self.frame, orient=HORIZONTAL,
+                mode='determinate', variable=self.runprogress)
+        self.progress.grid(row=5,column=0,columnspan=2)
+        self.timecourse = Canvas(self.frame)
+        self.timecourse.grid(row=6,column=0,columnspan=2)
 
-notebook = ttk.Notebook(root)
-operation = ttk.Frame(notebook)
-filemanage = ttk.Frame(notebook)
-plotting = ttk.Frame(notebook)
-notebook.add(operation, text="Spectrometer")
-notebook.add(filemanage, text="CSV")
-notebook.add(plotting, text="Plot")
-notebook.grid(row=0, column=0)
-
-def specselect(*args):
-    messagebox.showinfo(message="This will be used to select the spectrometer.")
-    return
-
-ttk.Label(operation, text="Spectrometer").grid(row=0,column=0)
-speccombo = ttk.Combobox(operation)
-speccombo['values'] = ('/dev/usb.serial', '/dev/ttyS0')
-speccombo.bind('<<ComboboxSelected>>', specselect)
-speccombo.grid(row=0,column=1)
-
-wavelength = StringVar()
-ttk.Label(operation, text="Wavelength (nm)").grid(row=1,column=0)
-waveentry = ttk.Entry(operation, textvariable=wavelength)
-waveentry.grid(row=1,column=1)
-
-frequency = StringVar()
-ttk.Label(operation, text="Frequency (s)").grid(row=2,column=0)
-freqentry = ttk.Entry(operation, textvariable=frequency)
-freqentry.grid(row=2,column=1)
-
-duration = StringVar()
-ttk.Label(operation, text="Duration (s)").grid(row=3, column=0)
-durentry = ttk.Entry(operation, textvariable=duration)
-durentry.grid(row=3,column=1)
-
-def blank(*args):
-    messagebox.showinfo(message="This will set the wavelength and blank the spectrometer.")
-    return
-
-blankbutton = ttk.Button(operation, text="Blank", command=blank)
-blankbutton.grid(row=4,column=0)
-
-def collect(*args):
-    messagebox.showinfo(message="This will collect data from the spectrometer for the specified period of time.")
-    return
-
-runbutton = ttk.Button(operation, text="Run", command=collect)
-runbutton.grid(row=4,column=1)
-
-runprogress = DoubleVar()
-progress = ttk.Progressbar(operation, orient=HORIZONTAL,
-        mode='determinate', variable=runprogress)
-progress.grid(row=5,column=0,columnspan=2)
-
-timecourse = Canvas(operation)
-timecourse.grid(row=6,column=0,columnspan=2)
-
-csvfile = StringVar()
-writerfields = []
-promptentries = {}
-promptstrings = {}
-promptlabels = {}
-def createnewfile(*args):
-    additional = additionaltext.get("1.0","end").split(",\n")
-    if analysismode.get() == "Time":
-        writerfields = ["Reaction"] + additional + ["Time","Abs"]
-    elif analysismode.get() == "Slope":
-        writerfields = ["Reaction"] + additional + ["Slope","Slope.Err"]
-    else:
-        # analysismode has not been set to a correct value
-        messagebox.showerror(message="Please set mode of recording.")
+    def specselect(*args):
+        messagebox.showinfo(message="This will be used to select the spectrometer.")
         return
-    with open(csvfile.get(), "w") as fileref:
-        csvdict = DictWriter(fileref, fields=writerfields)
-        csvdict.write_header()
-    reactionnumber.set(0)
-    newfile.destroy()
-    return
-def filenew(*args):
-    # New file. Prompt for info from dialog.
-    csvfile.set(filedialog.asksaveasfilename())
-    newfile = Toplevel()
-    newfile.title("New File")
-    newframe = ttk.Frame(newfile)
-    newframe.grid()
-    analysistime = ttk.Radiobutton(newframe, text="Raw",
-            variable=analysismode, value="Time")
-    analysistime.grid(row=0, column=0)
-    analysisslope = ttk.Radiobutton(newframe, text="Linear",
-            variable=analysismode, value="Slope")
-    analysisslope.grid(row=0, column=1)
-    ttk.Label(newframe, text="Additional Columns").grid(row=1,column=0)
-    additionaltext = Text(newframe, width=40, height=10)
-    additionaltext.grid(row=1, column=1)
-    cancelnew = ttk.Button(newframe, text="Cancel",
-            command=newfile.destroy)
-    cancelnew.grid(row=2, column=0)
-    savenewbutton = ttk.Button(newframe, text="Create",
-            command=createnewfile)
-    savenewbutton.grid(row=2, column=1)
-    return
-def fileselect(*args):
-    csvfile.set(filedialog.askopenfilename())
-    with open(csvfile.get(), "r") as fileref:
-        csvstart = fileref.read(1024)
-        if len(csvstart) > 0:
-            if not Sniffer().has_header(csvstart):
-                messagebox.showinfo(icon="error",
-                        message="CSV file contains content but no header!")
-                return
-            fileref.seek(0)
-            csvdict = DictReader(fileref)
-            # Determine what mode should be set for data writing
-            if "Abs" in csvdict.fieldnames:
-                # CSV holds unprocessed data
-                writerfields = csvdict.fieldnames
-                analysismode.set("Time")
-                additional = [field for field in writerfields if field not in ["Reaction","Time","Abs"]]
-                for prow, field in enumerate(additional):
-                    promptstrings[field] = StringVar()
-                    promptlabels[field] = ttk.Label(filemanage, text=field)
-                    promptlabels[field].grid(row=3+prow, column=0)
-                    promptentries[field] = ttk.Entry(filemanage,
-                            textvariable=promptstrings[field])
-                    promptentries[field].grid(row=3+prow, column=1)
-                for entry in csvdict:
-                    reactionnumber.set(entry["Reaction"])
-                    for field in additional:
-                        promptstrings[field] = entry[field]
-            elif "Slope" in csvdict.fieldnames:
-                writerfields = csvdict.fieldnames
-                analysismode.set("Slope")
-                additional = [field for field in writerfields if field != "Reaction"]
-                for prow, field in enumerate(additional):
-                    promptstrings[field] = StringVar()
-                    promptlabels[field] = ttk.Label(filemanage, text=field)
-                    promptlabels[field].grid(row=3+prow, column=0)
-                    promptentries[field] = ttk.Entry(filemanage,
-                            textvariable=promptstrings[field])
-                    promptentries[field].grid(row=3+prow, column=1)
-                for entry in csvdict:
-                    reactionnumber.set(entry["Reaction"])
-                    for field in additional:
-                        promptstrings[field] = entry[field]
-            else:
-                # Headers don't correspond to a set generated by this program
-                messagebox.showerror(message="CSV file doesn't match those generated by this program.")
+
+    def blank(*args):
+        messagebox.showinfo(message="This will set the wavelength and blank the spectrometer.")
+        return
+
+    def collect(*args):
+        messagebox.showinfo(message="This will collect data from the spectrometer for the specified period of time.")
+        return
+
+
+class CreateNewFile():
+    def __init__(self, parent, filename):
+        self.parent = parent
+        self.filename = filename
+        self.newfile = Toplevel()
+        self.newfile.title("New File")
+        self.newframe = ttk.Frame(newfile)
+        self.newframe.grid()
+        self.analysistime = ttk.Radiobutton(self.newframe, text="Raw",
+                variable=self.analysismode, value="Time")
+        self.analysistime.grid(row=0, column=0)
+        self.analysisslope = ttk.Radiobutton(self.newframe, text="Linear",
+                variable=self.analysismode, value="Slope")
+        self.analysisslope.grid(row=0, column=1)
+        ttk.Label(newframe, text="Additional Columns").grid(row=1,column=0)
+        self.additionaltext = Text(self.newframe, width=40, height=10)
+        self.additionaltext.grid(row=1, column=1)
+        self.cancelnew = ttk.Button(self.newframe, text="Cancel",
+                command=self.newfile.destroy)
+        self.cancelnew.grid(row=2, column=0)
+        self.savenewbutton = ttk.Button(self.newframe, text="Create",
+                                   command=self.savefile)
+        self.savenewbutton.grid(row=2, column=1)
+    def savefile(*args):
+        additional = self.additionaltext.get("1.0","end").split(",\n")
+        if self.analysismode.get() == "Time":
+            parent.writerfields = ["Reaction"] + additional + ["Time","Abs"]
+        elif analysismode.get() == "Slope":
+            parent.writerfields = ["Reaction"] + additional + ["Slope","Slope.Err"]
         else:
-            messagebox.showerror(message="CSV file doesn't contain any content.")
-    return
+            # analysismode has not been set to a correct value
+            messagebox.showerror(message="Please set mode of recording.")
+            return
+        with open(self.filename, "w") as fileref:
+            csvdict = DictWriter(fileref, fields=parent.writerfields)
+            csvdict.write_header()
+        parent.reactionnumber.set(0)
+        self.newfile.destroy()
+        return
 
-ttk.Label(filemanage, textvariable=csvfile).grid(row=0,column=0,columnspan=2)
+class FileTab():
+    def __init__(self, parent):
+        self.parent = parent
+        self.frame = ttk.Frame(parent.notebook)
+        self.parent.notebook.add(self.frame, text="CSV")
+        self.csvfile = StringVar()
+        self.writerfields = []
+        self.promptentries = {}
+        self.promptstrings = {}
+        self.promptlabels = {}
+        ttk.Label(self.frame, textvariable=self.csvfile).grid(row=0,
+                                                              column=0,
+                                                              columnspan=2)
+        self.filebutton = ttk.Button(self.frame, text="Select...",
+                                     command=self.fileselect)
+        self.filebutton.grid(row=1,column=0)
+        self.newbutton = ttk.Button(self.frame, text="New File",
+                                    command=self.filenew)
+        self.newbutton.grid(row=1,column=1)
+        self.analysismode = StringVar()
+        self.reactionnumber = IntVar()
+        ttk.Label(self.frame, text="Reaction").grid(row=2,column=0)
+        self.reactionlabel = ttk.Label(self.frame,
+                                       textvariable=self.reactionnumber)
+        self.reactionlabel.grid(row=2,column=1)
+        self.writebutton = ttk.Button(self.frame, text="Save",
+                                      command=self.writelines)
+        self.writebutton.grid(row=13,column=0)
+    def filenew(*args):
+        # New file. Prompt for info from dialog.
+        filename = filedialog.asksaveasfilename()
+        CreateNewFile(self, filename)
+        return
+    def fileselect(*args):
+        filename = filedialog.askopenfilename()
+        with open(filename, "r") as fileref:
+            csvstart = fileref.read(1024)
+            if len(csvstart) > 0:
+                if not Sniffer().has_header(csvstart):
+                    messagebox.showinfo(icon="error",
+                            message="CSV file contains content but no header!")
+                    return
+                fileref.seek(0)
+                csvdict = DictReader(fileref)
+                # Determine what mode should be set for data writing
+                if "Abs" in csvdict.fieldnames:
+                    # CSV holds unprocessed data
+                    self.csvfile.set(filename)
+                    self.writerfields = csvdict.fieldnames
+                    self.analysismode.set("Time")
+                    additional = [field for field in writerfields if field not in ["Reaction","Time","Abs"]]
+                    for prow, field in enumerate(additional):
+                        self.promptstrings[field] = StringVar()
+                        self.promptlabels[field] = ttk.Label(self.frame,
+                                                             text=field)
+                        self.promptlabels[field].grid(row=3+prow, column=0)
+                        self.promptentries[field] = ttk.Entry(self.frame,
+                                textvariable=self.promptstrings[field])
+                        self.promptentries[field].grid(row=3+prow, column=1)
+                    for entry in csvdict:
+                        self.reactionnumber.set(entry["Reaction"])
+                        for field in additional:
+                            self.promptstrings[field] = entry[field]
+                elif "Slope" in csvdict.fieldnames:
+                    self.csvfile.set(filename)
+                    self.writerfields = csvdict.fieldnames
+                    self.analysismode.set("Slope")
+                    additional = [field for field in writerfields if field != "Reaction"]
+                    for prow, field in enumerate(additional):
+                        self.promptstrings[field] = StringVar()
+                        self.promptlabels[field] = ttk.Label(self.frame,
+                                                             text=field)
+                        self.promptlabels[field].grid(row=3+prow, column=0)
+                        self.promptentries[field] = ttk.Entry(self.frame,
+                                textvariable=self.promptstrings[field])
+                        self.promptentries[field].grid(row=3+prow, column=1)
+                    for entry in csvdict:
+                        self.reactionnumber.set(entry["Reaction"])
+                        for field in additional:
+                            self.promptstrings[field] = entry[field]
+                else:
+                    # Headers don't correspond to a set generated by this program
+                    messagebox.showerror(message="CSV file doesn't match those generated by this program.")
+            else:
+                messagebox.showerror(message="CSV file doesn't contain any content.")
+        return
 
-filebutton = ttk.Button(filemanage, text="Select...", command=fileselect)
-filebutton.grid(row=1,column=0)
+    def writelines(*args):
+        messagebox.showinfo(message="This button will trigger writing to the CSV file.")
+        return
 
-newbutton = ttk.Button(filemanage, text="New File", command=filenew)
-newbutton.grid(row=1,column=1)
+class PlotTab():
+    def __init__(self, parent):
+        self.parent = parent
+        self.frame = ttk.Frame(self.parent.notebook)
+        self.parent.notebook.add(self.frame, text="Plot")
+        ttk.Label(self.frame, text="Plot slope as a function of").grid(row=0,
+                                                                       column=0)
+        self.plotx = StringVar()
+        self.plotcombo = ttk.Combobox(self.frame, textvariable=self.plotx)
+        self.plotcombo.bind('<<ComboboxSelected>>', self.plotanalysis)
+        self.plotcombo['values'] = ('To', 'Be', 'Computed')
+        self.plotcombo.grid(row=0,column=1)
+        self.analysisplot = Canvas(self.frame)
+        self.analysisplot.grid(row=11,column=0,columnspan=2)
 
-analysismode = StringVar()
-reactionnumber = IntVar()
+    def plotanalysis(*args):
+        messagebox.showinfo(message="The analysis plot should be updated now.")
+        return
 
-ttk.Label(filemanage, text="Reaction").grid(row=2,column=0)
-reactionlabel = ttk.Label(filemanage, textvariable=reactionnumber)
-reactionlabel.grid(row=2,column=1)
-
-def writelines(*args):
-    messagebox.showinfo(message="This button will trigger writing to the CSV file.")
-    return
-
-writebutton = ttk.Button(filemanage, text="Save", command=writelines)
-writebutton.grid(row=13,column=0)
-
-ttk.Label(plotting, text="Plot slope as a function of").grid(row=0,column=0)
-
-def plotanalysis(*args):
-    messagebox.showinfo(message="The analysis plot should be updated now.")
-    return
-
-plotx = StringVar()
-plotcombo = ttk.Combobox(plotting, textvariable=plotx)
-plotcombo.bind('<<ComboboxSelected>>', plotanalysis)
-plotcombo['values'] = ('To', 'Be', 'Computed')
-plotcombo.grid(row=0,column=1)
-
-analysisplot = Canvas(plotting)
-analysisplot.grid(row=11,column=0,columnspan=2)
-
+class KineticsGUI():
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Absorbance Kinetics")
+        self.notebook = ttk.Notebook(root)
+        self.notebook.grid(row=0, column=0)
+        self.operation = SpecTab(self)
+        self.filemanage = FileTab(self)
+        self.plotting = PlotTab(self)
+        
+root = Tk()
+KineticsGUI(root)
 root.mainloop()
